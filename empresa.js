1b221cafef1564058d1ccaf0686ee43284220c78
edsonBJR <ebjr.tux@gmail.com>
2020-06-16 08:56:06 -0300

    Atualizado arquivo empresa.js

---
+++ empresa.js
@@ -0,0 +1,11 @@
+const empresa = {
+    name: "Rocketseat",
+    color: "Roxo",
+    foco: "Programação",
+    address: {
+        street: "Rua Guilherme Gembala",
+        number: "260"
+    }
+}
+
+console.log(`A empresa ${empresa.name} está localizada em ${empresa.address.street}, ${empresa.address.number}`)
